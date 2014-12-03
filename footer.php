dec4e31ee1716bfaa9acdee7c636731120c49ae0
Vijay Menon <vijaymenonx@gmail.com>
2014-12-03 12:45:20 -0800

    Fix Footer

--- footer.php
+++ footer.php
@@ -1,4 +1,24 @@
+<div id="contact-area-m">
+	<div class="socialmedia-m">
+		<ul class="socialicons">
+			<li><a target="_blank" href="mailto:info@preened.com"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/email.jpg" /></a></li>
+			<li><a target="_blank" href="http://instagram.com/preeninc"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/in.jpg" /></a></li>
+			<li><a target="_blank" href="http://www.pinterest.com/preeninc/"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/pn.jpg" /></a></li>
+			<li><a target="_blank" href="https://www.facebook.com/preeninc"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/fb.jpg" /></a></li>
+			<li><a target="_blank" href="https://plus.google.com/u/0/b/114939697687431501947/+PreenIncLosAngeles/about"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/gp.jpg" /></a></li>
+			<li><a target="_blank" href="https://www.linkedin.com/company/preen-inc-"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/ln.jpg" /></a></li>
+			<li><a target="_blank" href="http://www.youtube.com/user/preeninc"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/yt.jpg" /></a></li>
+			<li><a target="_blank" href="https://twitter.com/preeninc"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/tw.jpg" /></a></li>
+			<li><a target="_blank" href="https://soundcloud/preeninc"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/sn.jpg" /></a></li>
+		</ul>
+	</div>
+	<div id="google-map" onclick="location.href='#';" style="cursor: pointer;">
 
+	<a target="_blank" href="https://www.google.com/maps/preview?ie=UTF-8&fb=1&gl=us&sll=34.0654793,-118.238679&sspn=0.0273035,0.0439464&q=831+Chung+King+Rd,+Los+Angeles,+CA+90012&ei=cOkHVNPaM8SuggTulIC4AQ&ved=0CB8Q8gEwAA">831 CHUNG KING ROAD
+	LOS ANGELES, CA 90012</a>
+	TEL +1 (213) 625-2100
+	</div>
+	</div>
 </div><!--#wrapper-->
 </div><!--#main_body-->
 <br style="clear:both;" />
